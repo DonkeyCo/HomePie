0b40f4516cd3c62a795cae5687bf905632ee8b57
DonkeyCo <duc.vongoc01@gmail.com>
2019-05-15 00:03:12 +0200

    Add speech recognition module, which runs in main now

--- homepie/__main__.py
+++ homepie/__main__.py
@@ -1,4 +1,6 @@
 import configparser
+import time
+from homepie.mods.sound_mod.recognition import Recognition
 
 
 def main():
@@ -7,6 +9,12 @@ def main():
 
     news_api_key = config["NEWS"]["API_KEY"]
     spotify_client_secret = config["SPOTIFY"]["CLIENT_SECRET"]
+
+    r = Recognition(config)
+    r.listen_background()
+
+    while True:
+        time.sleep(0.1)
 
 
 def init():
---
+++ homepie/mods/sound_mod/recognition.py
@@ -0,0 +1,21 @@
+import speech_recognition as sr
+
+
+class Recognition:
+
+    def __init__(self, config=None):
+        self.config = config
+        self.r = sr.Recognizer()
+
+    def listen_background(self):
+        self.r.listen_in_background(sr.Microphone(), self.get_text)
+
+    def get_text(self, recognizer, audio):
+        text = ""
+        try:
+            text = self.r.recognize_wit(audio, self.config["WIT"]["API_KEY"])
+            print(text)
+        except sr.UnknownValueError:
+            print("Wit.ai could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Wit.ai service; {0}".format(e))
