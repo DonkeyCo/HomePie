c8ba729b4668e3a2df574e27e752627e0eee1c72
DonkeyCo <duc.vongoc01@gmail.com>
2019-05-24 10:22:15 +0200

    ability to continue playback

--- homepie/__main__.py
+++ homepie/__main__.py
@@ -2,17 +2,15 @@ import configparser
 import time
 from homepie.mods.sound_mod.recognition import Recognition
 from homepie.mods.music_mod.spotify import Spotify
+import homepie.mods.weather_mod.weather as weather
 
 
 def main():
     config = configparser.ConfigParser()
     config.read('config.ini')
 
-    news_api_key = config["NEWS"]["API_KEY"]
-    spotify_client_secret = config["SPOTIFY"]["CLIENT_SECRET"]
-
     s = Spotify(config)
-    s.test()
+    s.test_play()
 
 
 def init():
--- homepie/mods/music_mod/spotify.py
+++ homepie/mods/music_mod/spotify.py
@@ -7,32 +7,11 @@ class Spotify:
 
     def __init__(self, config=None):
         self.config = config
+        self.scope = "user-library-read streaming"
+        self.token = util.prompt_for_user_token("donkeyco", self.scope, self.config["SPOTIFY"]["CLIENT_ID"],
+                                                self.config["SPOTIFY"]["CLIENT_SECRET"], self.config["SPOTIFY"]["REDIRECT"])
+        self.spotify = spotipy.Spotify(auth=self.token)
 
-    def show_tracks(self, tracks):
-        for i, item in enumerate(tracks['items']):
-            track = item['track']
-            print("%d %32.32s %s" % (i, track['artists'][0]['name'],track['name']))
-
-    def test(self):
-        scope = "user-library-read"
-        token = util.prompt_for_user_token("donkeyco", scope, self.config["SPOTIFY"]["CLIENT_ID"],
-                                           self.config["SPOTIFY"]["CLIENT_SECRET"], self.config["SPOTIFY"]["REDIRECT"])
-        if token:
-            sp = spotipy.Spotify(auth=token)
-            playlists = sp.user_playlists("donkeyco")
-            for playlist in playlists['items']:
-                if playlist['owner']['id'] == "donkeyco":
-                    print
-                    print
-                    playlist['name']
-                    print
-                    '  total tracks', playlist['tracks']['total']
-                    results = sp.user_playlist("donkeyco", playlist['id'],
-                                               fields="tracks,next")
-                    tracks = results['tracks']
-                    self.show_tracks(tracks)
-                    while tracks['next']:
-                        tracks = sp.next(tracks)
-                        self.show_tracks(tracks)
-        else:
-            print("fuck")
+    def test_play(self):
+        print(self.token)
+        self.spotify.start_playback()
